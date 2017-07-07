bf6fd29527d49a737919bcb76a3566eb3ff5d84b
strongwarmi <elianasc10@hotmail.com>
2017-07-07 16:00:40 -0500

    header desktop y mobile terminado

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -4,35 +4,33 @@
 const render = (root) => {
     root.empty();
     root.append(Header());
-    //root.append(Dashboard());
+    
 };
 
 const state = {
-    pin: null,
+    screen: null,
 
 };
 
 $(_ => {
-/*  getJSON('https://api.pinterest.com/v1/boards/arabelyuska/web-ui/pins/?access_token=AQA6SxA52Kr8rZEppfw-TgqsLSA6FM4qoiPYYN1EI1rXoUA7AwAAAAA&fields=id%2Clink%2Cnote%2Curl%2Cmedia%2Cattribution%2Cboard%2Cmetadata%2Ccolor%2Ccounts%2Coriginal_link%2Ccreated_at%2Ccreator%2Cimage', (err, json) => {
-    if (err) {
-        return alert(err.message);
-    }
-    state.pin = json;
-
-  });*/
-        const root = $('.root');
+    const root = $('.root');
     render(root);
 });
 
 'use strict';
 const Header = () => {
-    const header = $('<header></header>');
-    const row = $('<div class="row"></div>');
-    const content = $('<div id="logo" class="col-sm-1 text-center"><a href="index.html"><img width="35" src="assets/img/pinterest-logo.png" alt="Pinterest" title="Pinterest" /></a></div><div class="col-sm-9 buscar"><i class="glyphicon glyphicon-search"></i><input type="text" placeholder="Buscar"></div><div class="col-sm-2 text-center"><img width="34" src="assets/img/icon-arrow.png" /><img width="34" src="assets/img/icon-profile.png" /><img width="34" src="assets/img/message.png" /></div>');
+    const header = $('<header class="container"></header>');
+    
+    const navDesktop = $('<div class="row hidden-xs head-item search"><div class="col-sm-2"><img src="assets/img/menu.png"> SECTIONS</div><div class="col-sm-2"><img src="assets/img/search.png"> SEARCH</div><div class="col-sm-2 col-sm-offset-6 redes"><img src="assets/img/fb.png"><img src="assets/img/tw.png"><img src="assets/img/in.png"></div></div><div class="row hidden-xs text-center head-item"><img src="assets/img/logoicon.png" alt=""><br><br><p>Lunes, Junio 12 de 2017 | <img src="assets/img/cloud.png"> 22°</p></div>');
+    
+    const menu = $('<div class="row menu hidden-xs"><ul><li>Lo último</li><li>Opinión </li><li>Cultura</li><li>Perú</li>     <li>Tecnologia</li><li>Mundo</li><li>Economía</li><li>Lifestyle</li><li>Deporte</li></ul></div>');
+    
+    const navMobile = $('<div class="row hidden-sm hidden-md hidden-lg nav-mobile"><div class="col-xs-8"><img src="assets/img/logoicon.png" class="text-left" alt="Laboratoria" height="36"></div><div class="col-xs-2 col-xs-offset-2"><img src="assets/img/menu.png" height="25"></div></div>');
 
-    row.append(content);
-    header.append(row);
-
+    header.append(navDesktop);
+    header.append(menu)
+    header.append(navMobile);
+    
     return header;
 }
 
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,21 +3,15 @@
 const render = (root) => {
     root.empty();
     root.append(Header());
-    //root.append(Dashboard());
+    
 };
 
 const state = {
-    pin: null,
+    screen: null,
 
 };
 
 $(_ => {
-  getJSON('https://api.pinterest.com/v1/boards/arabelyuska/web-ui/pins/?access_token=AQA6SxA52Kr8rZEppfw-TgqsLSA6FM4qoiPYYN1EI1rXoUA7AwAAAAA&fields=id%2Clink%2Cnote%2Curl%2Cmedia%2Cattribution%2Cboard%2Cmetadata%2Ccolor%2Ccounts%2Coriginal_link%2Ccreated_at%2Ccreator%2Cimage', (err, json) => {
-    if (err) {
-        return alert(err.message);
-    }
-    state.pin = json;
     const root = $('.root');
     render(root);
-  });
 });
